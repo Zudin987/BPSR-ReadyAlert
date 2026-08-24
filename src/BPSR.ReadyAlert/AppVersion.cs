1cd884309eff16544e2d70cf2de719ae7f67f1cb
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-24 13:02:00 +0800

    Update fallback app version

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -5,5 +5,5 @@ namespace BPSR.ReadyAlert;
 internal static class AppVersion
 {
     internal static string Current =>
-        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.4.2";
+        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.5.0";
 }
