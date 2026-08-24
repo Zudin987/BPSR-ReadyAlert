70a6e459b88ce5045eec0c915007ee395a31a2ed
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-24 16:19:09 +0800

    Set app version fallback to 1.0.0

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -5,5 +5,5 @@ namespace BPSR.ReadyAlert;
 internal static class AppVersion
 {
     internal static string Current =>
-        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.9.1";
+        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.0.0";
 }
