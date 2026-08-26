1b1fcc54cd95e7e39780b23e8d0a47caadf3a38e
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-26 11:57:52 +0800

    fix(version): show RC informational version

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -4,6 +4,20 @@ namespace BPSR.ReadyAlert;
 
 internal static class AppVersion
 {
-    internal static string Current =>
-        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.0";
+    internal static string Current
+    {
+        get
+        {
+            var informational = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+            if (!string.IsNullOrWhiteSpace(informational))
+            {
+                var plus = informational.IndexOf('+');
+                return plus >= 0 ? informational[..plus] : informational;
+            }
+
+            return Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.0-rc.2";
+        }
+    }
 }
