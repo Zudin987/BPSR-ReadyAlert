7944b17874fb19cd4c1796e5d98ed1f2940c1cf4
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-27 00:21:00 +0800

    Finalize v1.1.1 application version

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -1,23 +1,6 @@
-using System.Reflection;
-
 namespace BPSR.ReadyAlert;
 
 internal static class AppVersion
 {
-    internal static string Current
-    {
-        get
-        {
-            var informational = Assembly.GetExecutingAssembly()
-                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                .InformationalVersion;
-            if (!string.IsNullOrWhiteSpace(informational))
-            {
-                var plus = informational.IndexOf('+');
-                return plus >= 0 ? informational[..plus] : informational;
-            }
-
-            return Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.1-rc.2";
-        }
-    }
+    internal const string Current = "1.1.1";
 }
