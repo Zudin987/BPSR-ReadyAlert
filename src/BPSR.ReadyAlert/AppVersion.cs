708bda2e5e508f9261091596e9a6ea6c22576f11
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-26 15:52:01 +0800

    RC10: update fallback version

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -17,7 +17,7 @@ internal static class AppVersion
                 return plus >= 0 ? informational[..plus] : informational;
             }
 
-            return Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.0-rc.9";
+            return Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.0-rc.10";
         }
     }
 }
