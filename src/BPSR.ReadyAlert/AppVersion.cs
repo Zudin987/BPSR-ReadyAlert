fd08d986409b7cd9545c5c49a57ff1aae8baedb6
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-26 11:38:21 +0800

    version: prepare v1.1.0 chat RC

--- src/BPSR.ReadyAlert/AppVersion.cs
+++ src/BPSR.ReadyAlert/AppVersion.cs
@@ -5,5 +5,5 @@ namespace BPSR.ReadyAlert;
 internal static class AppVersion
 {
     internal static string Current =>
-        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.0.0";
+        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.1.0";
 }
