5761b07bf7eb2203ae07228d0951feb0ee2082ba
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-13 17:44:21 +0800

    Expose generated Benchmark UI module

--- rust/src/telemetry_v1260.rs
+++ rust/src/telemetry_v1260.rs
@@ -1,6 +1,10 @@
 #[path = "ui_audit_v1302.rs"]
 pub(crate) mod ui_audit_v1302;
 
+pub mod benchmark_ui {
+    include!(concat!(env!("OUT_DIR"), "/benchmark_ui_v1302.rs"));
+}
+
 #[path = "game_names_v1300.rs"]
 pub(crate) mod game_names_v1300;
 
