88e71a86234d7da4a91498ffc7310586987c75ed
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-13 11:57:50 +0800

    Load v1.30 catalog as a normal Rust module

--- rust/src/telemetry_v1260.rs
+++ rust/src/telemetry_v1260.rs
@@ -2,8 +2,7 @@ pub mod benchmark_ui {
     include!("benchmark_ui.rs");
 }
 
-pub(crate) mod game_names_v1300 {
-    include!("game_names_v1300.rs");
-}
+#[path = "game_names_v1300.rs"]
+pub(crate) mod game_names_v1300;
 
 include!("telemetry_v1270.rs");
