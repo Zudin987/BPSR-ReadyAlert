380631ff685be06fc03768ae3d3f9a930e5eb6cd
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-14 14:06:33 +0800

    fix: run final Season 4 future-proof follow-up

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,5 @@
 mod current {
-    include!("build/legacy/build_v1320_season4_future_proof.rs");
+    include!("build/legacy/build_v1320_season4_future_proof_fix.rs");
     pub fn run() { main(); }
 }
 
