c113eab685ae3dc2adbd13c443e6ba0112e3381c
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-14 15:02:37 +0800

    Run v1.32.1 freeze hardening stage

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,5 @@
 mod current {
-    include!("build/legacy/build_v1320_season4_future_proof_fix.rs");
+    include!("build/legacy/build_v1321_freeze_hardening.rs");
     pub fn run() { main(); }
 }
 
