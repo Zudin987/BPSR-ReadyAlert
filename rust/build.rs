23b460b4779c68456c8e92f10aeacc165d552a46
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-17 20:45:29 +0800

    Enable final narrow-header fit pass

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1344_meter_header_history_ux.rs");
+    include!("build/legacy/build_v1345_narrow_header_fit.rs");
     pub fn run() { main(); }
 }
 
