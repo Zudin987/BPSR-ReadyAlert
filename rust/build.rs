2fbf7e0379db76bac5c61cd944ae5eec24da0fb9
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-18 00:27:50 +0800

    Activate Raid hitbox alignment and sorting tooltip audit

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1352_audit_rank_gap.rs");
+    include!("build/legacy/build_v1353_audit_raid_hit_alignment.rs");
     pub fn run() { main(); }
 }
 
