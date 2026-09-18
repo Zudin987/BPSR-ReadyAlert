d8c6a3ead41d819fd187a138ff4233778a90e7a3
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-18 12:02:43 +0800

    build: activate raid height and mechanics auto-fit stage

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1359_non_meter_layout_fixes.rs");
+    include!("build/legacy/build_v1360_mode_height_rules.rs");
     pub fn run() { main(); }
 }
 
