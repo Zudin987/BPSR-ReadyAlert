d607ee0be59dd63b33a01c1b9cf003febf39bf21
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-18 12:43:24 +0800

    Wire restored detected Enrage header into generated Windows UI build

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1361_hit_test_type_fix.rs");
+    include!("build/legacy/build_v1362_enrage_header.rs");
     pub fn run() { main(); }
 }
 
