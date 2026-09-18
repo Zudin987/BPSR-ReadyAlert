e1dd3c009ebcd689bf4596046646997a55907d4c
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-18 10:51:03 +0800

    Enable final rank, death-count and meter-heading alignment stage

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1357_narrow_revive_labels.rs");
+    include!("build/legacy/build_v1358_rank_death_header_alignment.rs");
     pub fn run() { main(); }
 }
 
