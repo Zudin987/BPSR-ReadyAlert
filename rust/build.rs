52d15b981ebd43e8a68194ade42170cc2efdc152
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-20 20:08:45 +0800

    build: activate coordinated DPS scale audit stage

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1377_ui_audit_verified.rs");
+    include!("build/legacy/build_v1378_ui_audit_scale.rs");
     pub fn run() { main(); }
 }
 
