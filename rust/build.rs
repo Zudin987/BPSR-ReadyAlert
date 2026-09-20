75e6b0678e71562f5308f4c6c66457c87199b435
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 00:22:57 +0800

    build(ui): activate follow-up measurement regression fixes

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1385_followup_settings.rs");
+    include!("build/legacy/build_v1386_followup_test_contract.rs");
     pub fn run() { main(); }
 }
 
