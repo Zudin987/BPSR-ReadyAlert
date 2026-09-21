f989c7941683f5f9dd913415c411cacecc02ad55
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 22:33:53 +0800

    build: activate stale finalized-raid packet replay guard

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1406_manual_reset_once.rs");
+    include!("build/legacy/build_v1407_ignore_finished_run_replays.rs");
     pub fn run() { main(); }
 }
 
