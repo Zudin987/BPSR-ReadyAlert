bdd5ba2587bdf5bf7da68fbe55ee3ed8894c8b5a
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 22:16:41 +0800

    build: activate pre-entry run identity preservation

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1404_confirmed_raid_reentry.rs");
+    include!("build/legacy/build_v1405_preserve_preentry_run.rs");
     pub fn run() { main(); }
 }
 
