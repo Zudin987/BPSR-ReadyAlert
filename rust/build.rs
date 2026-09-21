b48d222ea1bd1069b6003317fffe4512a046540b
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 21:44:24 +0800

    Enable scene-consistent late-load lifecycle build stage

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1401_late_load_wipe.rs");
+    include!("build/legacy/build_v1402_scene_tracker_consistency.rs");
     pub fn run() { main(); }
 }
 
