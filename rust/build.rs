657e0bc21eefaa4998444df6455b7f8d8ba0e028
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 22:30:32 +0800

    build: activate manual reset history deduplication pre-release fix

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1405_preserve_preentry_run.rs");
+    include!("build/legacy/build_v1406_manual_reset_once.rs");
     pub fn run() { main(); }
 }
 
