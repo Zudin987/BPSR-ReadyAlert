9fae9de7fec20e47bcff7ee24ea341cdd8e1fb0b
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 21:36:35 +0800

    Enable late-load and wipe lifecycle generator

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1400_dead_identity_all_layouts.rs");
+    include!("build/legacy/build_v1401_late_load_wipe.rs");
     pub fn run() { main(); }
 }
 
