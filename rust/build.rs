2c6337a24b667472cbc5ee4a72e55af3467a0f99
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-09-21 11:15:43 +0800

    build: activate combo-popup monitoring and dark popup HWND pass

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 mod current {
-    include!("build/legacy/build_v1395_shape_surfaces_chrome.rs");
+    include!("build/legacy/build_v1396_combo_popup_workarea.rs");
     pub fn run() { main(); }
 }
 
